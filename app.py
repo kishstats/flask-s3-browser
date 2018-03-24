100978490d744eaa9822c251338b26cafc9268ea
Brett Kishkis <kish6731@gmail.com>
2018-03-23 19:20:58 -0700

    upload a file

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 import boto3
 from config import S3_BUCKET, S3_KEY, S3_SECRET
@@ -12,6 +12,7 @@ s3 = boto3.client(
 
 app = Flask(__name__)
 Bootstrap(app)
+app.secret_key = 'secret'
 app.jinja_env.filters['datetimeformat'] = datetimeformat
 app.jinja_env.filters['file_type'] = file_type
 
@@ -28,6 +29,18 @@ def files():
     summaries = my_bucket.objects.all()
 
     return render_template('files.html', my_bucket=my_bucket, files=summaries)
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files['file']
+
+    s3_resource = boto3.resource('s3')
+    my_bucket = s3_resource.Bucket(S3_BUCKET)
+    my_bucket.Object(file.filename).put(Body=file)
+
+    flash('File uploaded successfully')
+    return redirect(url_for('files'))
 
 
 if __name__ == "__main__":

995345fa84d7df1d010f85d5fc3e37ff6a6e523c
Brett Kishkis <kish6731@gmail.com>
2018-03-29 14:04:48 -0700

    part 5 list buckets

--- app.py
+++ app.py
@@ -1,15 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, \
     Response
 from flask_bootstrap import Bootstrap
-import boto3
-from config import S3_BUCKET, S3_KEY, S3_SECRET
 from filters import datetimeformat, file_type
-
-s3 = boto3.client(
-   "s3",
-   aws_access_key_id=S3_KEY,
-   aws_secret_access_key=S3_SECRET
-)
+from resources import get_bucket, get_buckets_list
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -20,13 +13,13 @@ app.jinja_env.filters['file_type'] = file_type
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    buckets = get_buckets_list()
+    return render_template("index.html", buckets=buckets)
 
 
 @app.route('/files')
 def files():
-    s3_resource = boto3.resource('s3')
-    my_bucket = s3_resource.Bucket(S3_BUCKET)
+    my_bucket = get_bucket()
     summaries = my_bucket.objects.all()
 
     return render_template('files.html', my_bucket=my_bucket, files=summaries)
@@ -36,8 +29,7 @@ def files():
 def upload():
     file = request.files['file']
 
-    s3_resource = boto3.resource('s3')
-    my_bucket = s3_resource.Bucket(S3_BUCKET)
+    my_bucket = get_bucket()
     my_bucket.Object(file.filename).put(Body=file)
 
     flash('File uploaded successfully')
@@ -48,8 +40,7 @@ def upload():
 def delete():
     key = request.form['key']
 
-    s3_resource = boto3.resource('s3')
-    my_bucket = s3_resource.Bucket(S3_BUCKET)
+    my_bucket = get_bucket()
     my_bucket.Object(key).delete()
 
     flash('File deleted successfully')
@@ -60,9 +51,7 @@ def delete():
 def download():
     key = request.form['key']
 
-    s3_resource = boto3.resource('s3')
-    my_bucket = s3_resource.Bucket(S3_BUCKET)
-
+    my_bucket = get_bucket()
     file_obj = my_bucket.Object(key).get()
 
     return Response(
---
+++ resources.py
@@ -0,0 +1,25 @@
+import boto3
+from config import S3_BUCKET, S3_KEY, S3_SECRET
+from pprint import pprint
+
+def _get_s3_resource():
+    if S3_KEY and S3_SECRET:
+        return boto3.resource(
+            's3',
+            aws_access_key_id=S3_KEY,
+            aws_secret_access_key=S3_SECRET
+        )
+    else:
+        return boto3.resource('s3')
+
+
+def get_bucket():
+    s3_resource = _get_s3_resource()
+    return s3_resource.Bucket(S3_BUCKET)
+
+
+def get_buckets_list():
+    client = boto3.client('s3')
+    buckets = client.list_buckets()
+
+    return client.list_buckets().get('Buckets')
